94b66f1fe899509cb4df091394e4fce84f6bbca3
lambdaplus <bond010418@gmail.com>
2016-04-02 14:08:28 +0800

    flasko代码

---
+++ Flasko.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+import os
+from flask.ext.script import Manager, Shell
+from flask import Flask, render_template, url_for, flash, redirect, abort, request, session
+from flask.ext.bootstrap import Bootstrap
+# from flask.ext.nav import Nav
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import Email, DataRequired, EqualTo, Length
+from flask.ext.login import LoginManager, login_user,login_required, logout_user, UserMixin, current_user
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from werkzeug.security import generate_password_hash, check_password_hash
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+app.config['SECRET_KEY'] = 'lambdaplus'
+bootstrap = Bootstrap(app)
+#nav = Nav(app)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
+class RegisterForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     EqualTo('confirm',
+                                                             message='Must be match ')])
+    confirm = PasswordField('Confirm password', validators=[DataRequired()])
+    accept_tos = BooleanField('I accept the TOS', [DataRequired()])
+
+    submit = SubmitField('Register Now')
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(),
+                                             Length(1, 64),
+                                             Email()])
+    #username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        #db.session.commit()
+        flash('Thanks for registering!')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or username')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('login'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    manager.run()
